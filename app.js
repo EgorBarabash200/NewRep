2a5bfea77fce88ee918f469570825e1ce84e2cbd
EgorBarabash200 <yegor.barabash.97@mail.ru>
2024-12-26 02:27:46 +0300

    Task_5 1.0

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-
 const input = document.getElementById('main-input');
 const btnTo = document.getElementById('main-button');
 const blockDivs = document.getElementById('div-block');
 let textInput = '';
-
 input.addEventListener("input", (event) => {
     textInput = event.target.value;
 });
-
 btnTo.addEventListener("click", (event) => {
     const div = document.createElement('div');
     div.textContent = textInput;
